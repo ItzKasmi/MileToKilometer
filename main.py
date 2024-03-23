cafeb4b338398dfc8a0ea0372d82299da4dc3f85
ItzKasmi <kasmi0328@gmail.com>
2024-03-23 16:26:39 -0400

    Finished Project

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ from tkinter import *
 
 # Window
 window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-
-# Label
-my_label = Label(text="I am a Label", font=("Arial", 24))
-my_label.pack()
-
-def change_text():
-    my_label["text"] = choice.get()
+window.title("Miles to Kilometer Converter")
+window.config(padx=15, pady=15)
 
 
-button = Button(text="Click Me", command=change_text)
-button.pack()
+def calculate():
+    choice = degrees.get()
+    result_text["text"] = int(choice) * 1.609
 
-choice = Entry(width=10)
-choice.pack()
+
+# Text
+miles_text = Label(text="Miles", font=("Arial", 12))
+equal_text = Label(text="is equal to", font=("Arial", 12))
+result_text = Label(text="0", font=("Arial", 12))
+km_text = Label(text="Km", font=("Arial", 12))
+
+# Text Placements
+miles_text.grid(row=0, column=3)
+equal_text.grid(row=1, column=0)
+result_text.grid(row=1, column=2)
+km_text.grid(row=1, column=3)
+
+# Input
+degrees = Entry(width=10)
+degrees.grid(row=0, column=2)
+
+# Button
+button = Button(text="Calculate", command=calculate)
+button.grid(row=2, column=2)
 
 window.mainloop()
